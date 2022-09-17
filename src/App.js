94b23f7a19275951f697ab711928f84e3c736991
Olga Krv <olgakrv@MacBook-Air-Olga.local>
2022-09-17 17:41:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import { GroceryList } from './GroceryList';
 function App() {
   return (
     <div className="app">
-         <div className='container'>
-      <img src={ image } width="200px" alt="shopping"/>
-      </div>
-       <div className='container'>
-      <h1>Grossery List</h1>
-      </div>
-      <GroceryList/>
-      <div className='container'>
-      <img src={ imageTwo } width="200px" alt="man" />
-      </div>
+        <div className='container'>
+          <img src={ image } width="200px" alt="shopping"/>
+        </div>
+        <div className='container'>
+          <h1>Grossery List</h1>
+        </div>
+        <GroceryList/>
+        <div className='container'>
+          <img src={ imageTwo } width="200px" alt="man" />
+        </div>
     </div>
   );
 }
--- src/GroceryList.js
+++ src/GroceryList.js
@@ -8,9 +8,7 @@ export class GroceryList extends Component {
     }
 
     onChangeEvent(e){
-        // console.log(e.target.value)
         this.setState({userInput: e});
-        // console.log(e)
     }
 
     addItem(input) {
@@ -21,55 +19,48 @@ export class GroceryList extends Component {
         let listArray = this.state.groceryList;
         listArray.push(input);
         this.setState({groceryList: listArray, userInput: '' })
-        // console.log(listArray)
         }
     }
 
     deleteItem() {
         let listArray = this.state.groceryList;
         listArray = [];
-        // listArray.length = 0; второй способ
         this.setState({groceryList: listArray})
     }
 
     crossedWord(event) {
         const li = event.target;
         li.classList.toggle('crossed');
-
     }
 
     onFormSubmit(e) {
         e.preventDefault();
     }
 
-
     render() {
         return(
-            // <div> в этом случае form заменяет общий div
                 <form onSubmit={this.onFormSubmit}>
-            <div className='container'>
-                <input type="text"
-                    placeholder="What do you want to buy today?"
-                    // onChange={this.onChangeEvent} 
-                    onChange={(e) => {this.onChangeEvent(e.target.value)}}
-                    value={this.state.userInput}/>
-            </div> 
-            <div className='container'>
-                <button onClick={() => this.addItem(this.state.userInput)} className='btn add'>Add</button>
-            </div>
-            <ul>
-                {this.state.groceryList.map((item, i) => (
-                    <li onClick={this.crossedWord} key={i}>
+                <div className='container'>
+                    <input type="text"
+                        placeholder="What do you want to buy today?"
+                        onChange={(e) => {this.onChangeEvent(e.target.value)}}
+                        value={this.state.userInput}/>
+                </div> 
+                <div className='container'>
+                    <button onClick={() => this.addItem(this.state.userInput)} className='btn add'>Add</button>
+                </div>
+                <ul>
+                    {this.state.groceryList.map((item, i) => (
+                        <li onClick={this.crossedWord} key={i}>
                         <img src={check} width="40px" alt="icon" />
                         {item}
-                    </li>
-                ))}
-            </ul>
-            <div className='container'>
-            <button onClick={() => this.deleteItem()} className='btn delete'>Delete</button>
-            </div>
-            </form>
-            // </div>
+                        </li>
+                    ))}
+                </ul>
+                <div className='container'>
+                    <button onClick={() => this.deleteItem()} className='btn delete'>Delete</button>
+                </div>
+                </form>
         )
     }
 }
